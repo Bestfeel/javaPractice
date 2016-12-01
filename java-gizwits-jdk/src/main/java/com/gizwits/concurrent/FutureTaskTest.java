43dfbd38fa9e5c24cb8c7e69c1b1571f68eeadc4
Feel <fye@xtremeprog.com>
2016-12-01 10:36:28 +0800

    update

--- java-gizwits-jdk/src/main/java/com/gizwits/concurrent/FutureTaskTest.java
+++ java-gizwits-jdk/src/main/java/com/gizwits/concurrent/FutureTaskTest.java
@@ -10,19 +10,32 @@ public class FutureTaskTest {
 
     public static void main(String[] args) throws InterruptedException, ExecutionException {
 
+
+        //  第一种方法：
         //构造FutureTask
         FutureTask<String> futureTask = new FutureTask<>(new CallData());
+
 
         ExecutorService service = Executors.newFixedThreadPool(1);
 
 
         service.submit(futureTask);
 
+
+        //  第二种方法：
+
+        CompletionService<String> completionService = new ExecutorCompletionService<>(service);
+        completionService.submit(new CallData());
+
+        System.out.println(completionService.take().get());
+
+
         service.shutdown();
 
         service.awaitTermination(100, TimeUnit.DAYS);
         //如果此时call()方法没有执行完成，则依然会等待
         System.out.println(futureTask.get());
+
 
     }
 }
